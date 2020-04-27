ceff94f86b854ed84c703b290bc21ab3bf2e4db8
ronnie-colmen <ronnie@softsquared.com>
2020-04-27 13:17:24 +0900

    nothing

--- pdos/DatabasePdo.php
+++ pdos/DatabasePdo.php
@@ -4,10 +4,10 @@
 function pdoSqlConnect()
 {
     try {
-        $DB_HOST = "127.0.0.1";
-        $DB_NAME = "TimelineDB";
-        $DB_USER = "test";
-        $DB_PW = "111111";
+        $DB_HOST = "3.34.80.36";
+        $DB_NAME = "Ably";
+        $DB_USER = "root";
+        $DB_PW = "jjw969587!";
         $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PW);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
